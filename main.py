482a62067ef6a3bf3a89556d2ed8abbc007b0ae9
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-08-18 16:40:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
-#ver 18082023_1515
+
 nltk.download('punkt')
 app = Flask(__name__)
 
@@ -26,8 +26,8 @@ def get_answer():
     logging.info(f"Received question: {user_question}")
 
     if "parrot" in user_question.lower():
-        logging.info(f"Best answer: {best_answer}")
         best_answer = get_best_answer(user_question, site_contents)
+         logging.info(f"Best answer: {best_answer}")
         return jsonify({"best_answer": best_answer})
     else:
         return jsonify({"message": "Not a parrot-related question."})
@@ -40,12 +40,19 @@ def calculate_similarity(query, sentences):
     return similarity_scores
 
 def get_best_answer(query, content_list):
-    sentences = []
-    for content in content_list:
-        sentences.extend(preprocess_text(content))
-    similarity_scores = calculate_similarity(query, sentences)
-    most_similar_index = np.argmax(similarity_scores)
-    return sentences[most_similar_index]
+    try:
+        sentences = []
+        for content in content_list:
+            sentences.extend(preprocess_text(content))
+        similarity_scores = calculate_similarity(query, sentences)
+        most_similar_index = np.argmax(similarity_scores)
+        return sentences[most_similar_index]
+    except Exception as error:
+                print("An error occurred:", error)
+                return {
+                    "statusCode": error.args[0],
+                    "body": json.dumps(f"An error occurred:, {error}")
+                    }
 
 def scrape_web_page(url):
     # web scraping logic 
@@ -56,42 +63,45 @@ def scrape_web_page(url):
         return "Failed to scrape content from " + url
 
 def lambda_handler(event, context):
-    print(event)
-    user_question = event["question"]
+    try:
+        print(event)
+        user_question = event["question"]
+        response = requests.post('https://0ai42tfv4e.execute-api.us-west-1.amazonaws.com/default/MyNLPSearchService', 
+                            json={"subject": user_question})
 
-    response = requests.post('https://0ai42tfv4e.execute-api.us-west-1.amazonaws.com/default/MyNLPSearchService', 
-                        json={"subject": user_question})
-
-    # Assuming you have fetched the related sites URLs
-    related_sites = response.body
-    #[
-    #    "https://www.example.com/site1",
-    #    "https://www.example.com/site2",
+        # Assuming you have fetched the related sites URLs
+        related_sites = response.body
+        #[
+        #    "https://www.example.com/site1",
+        #    "https://www.example.com/site2",
         # Add more URLs
-    #]
-
-    # Extract content from the related sites
-    site_contents = []
-    for url in related_sites:
-        content = scrape_web_page(url)
-        site_contents.append(content)
-
-    # Assuming you have determined that the user's question is related to parrots
-    if "parrot" in user_question.lower():
-        best_answer = get_best_answer(user_question, site_contents)
-	# Log the best answer
-        logging.info(f"Best answer: {best_answer}")
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"best_answer": best_answer})
-        }
-    else:
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"message": "Not a parrot-related question."})
-        }
-
-
+        #]
+        # Extract content from the related sites
+        site_contents = []
+        for url in related_sites:
+            content = scrape_web_page(url)
+            site_contents.append(content)
+        # Assuming you have determined that the user's question is related to parrots
+        if "parrot" in user_question.lower():
+            logging.info(f"User question: {user_question}")
+            best_answer = get_best_answer(user_question, site_contents)
+	        # Log the best answer
+            logging.info(f"Best answer: {best_answer}")
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"best_answer": best_answer})
+            }
+        else:
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"message": "Not a parrot-related question."})
+            }
+    except Exception as error:
+            print("An error occurred:", error)
+            return {
+                "statusCode": error.args[0],
+                "body": json.dumps(f"An error occurred:, {error}")
+                }
 
 if __name__ == '__main__':
     host = '0.0.0.0'
